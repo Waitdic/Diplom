5f606993af99bc4a9c41049437c604bd6019aeb2
Danis Yakupov <waitdic161616@gmail.com>
2021-07-03 14:27:36 +0300

    Доделываю тесты

---
+++ FunctionalTests/LoadPullTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using COPOL.BLL.Models;
+using FunctionalTests.Common;
+using NUnit.Framework;
+
+namespace FunctionalTests
+{
+    [TestFixture]
+    public class LoadPullTests
+    {
+        [TestCase(TestName = "Тест на правильный расчет точек. Позитивный тест.")]
+        public void LoadPull_CalculatePoint_CorrectResult()
+        {
+            // SetUp
+            var parameters = ParametersHelper.GetParameters();
+            var newParameters = new Parameters
+            {
+                Vds0 = parameters.Vds0,
+                Ids0 = (float) (parameters.Ids0 * Math.Pow(10, -3)),
+                Cgs = (float) (parameters.Cgs * Math.Pow(10, -12)),
+                Cds = (float) (parameters.Cds * Math.Pow(10, -12)),
+                Cgd = (float) (parameters.Cgd * Math.Pow(10, -12)),
+                Ls = (float) (parameters.Ls * Math.Pow(10, -9)),
+                Ld = (float) (parameters.Ld * Math.Pow(10, -9)),
+                Gm = (float) (parameters.Gm * Math.Pow(10, -3)),
+                Frequences = parameters.Frequences,
+                LoopP = parameters.LoopP,
+                N = parameters.N,
+                Step = parameters.Step,
+            };
+            
+            // Act
+            var builder = new LoadPull(newParameters, 50);
+            var outputPoints = builder.CalculatePoint();
+            
+            // Assert
+            Assert.NotNull(outputPoints);
+            Assert.NotNull(builder.PMaxOutput);
+        }
+        
+        [TestCase(TestName = "Тест присваиваения неправильных" +
+                             " данных для расчета точек. Негативный тест.")]
+        public void LoadPull_SetWrongParameters_CorrectResult()
+        {
+            // SetUp
+            var parameters = ParametersHelper.GetParameters();
+            var newParameters = new Parameters
+            {
+                Vds0 = parameters.Vds0,
+                Ids0 = (float) (parameters.Ids0 * Math.Pow(10, -3)),
+                Cgs = (float) (parameters.Cgs * Math.Pow(10, -12)),
+                Cds = (float) (parameters.Cds * Math.Pow(10, -12)),
+                Cgd = (float) (parameters.Cgd * Math.Pow(10, -12)),
+                Ls = (float) (parameters.Ls * Math.Pow(10, -9)),
+                Ld = (float) (parameters.Ld * Math.Pow(10, -9)),
+                Gm = (float) (parameters.Gm * Math.Pow(10, -3)),
+                Frequences = parameters.Frequences,
+                LoopP = parameters.LoopP,
+                N = parameters.N,
+                Step = 200,
+            };
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                // Act
+                var builder = new LoadPull(newParameters, 50);
+                builder.CalculatePoint();
+            });
+        }
+    }
+}
---
+++ FunctionalTests/SmithChartTests.cs
@@ -0,0 +1,61 @@
+﻿/*using COPOL.BLL.Models;
+using NUnit.Framework;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FunctionalTests
+{
+    [TestFixture]
+    public class SmithChartTests
+    {
+        private readonly PictureBox _graphics = new();
+        
+        // Центры горизонтальных окружностей. 
+        private static readonly float[] _arg2 = {
+            0.0f, 0.2f, 0.4f, 0.6f,
+            0.8f, 1.0f, 1.4f, 1.8f, 
+            2.2f, 3.0f, 4.0f, 5.0f, 10.0f
+        };
+        
+        // Центры Вертикальных окружностей. 
+        private static readonly float[] _arg1 = { 
+            0.2f, 0.4f, 0.6f, 0.8f, 1.0f,
+            1.2f, 1.4f, 1.6f, 1.9f, 2.3f,
+            3.0f, 4.0f, 5.0f, 10.0f
+        };
+        
+        private void Initial()   
+        {
+            _graphics.BackColor = Color.White;
+            _graphics.Location = new Point(12, 7);
+            _graphics.Name = "SmithChart";
+            _graphics.Size = new Size(540, 540);
+            _graphics.TabIndex = 11;
+            _graphics.TabStop = false;
+            _graphics.Paint += CreateGraphics_Paint;
+        }
+        
+        [TestCase(TestName = "Тест для проверки рисования диаграммы Смита.")]
+        public void SmithChart_DrawGraphics_CorrectResult()
+        {
+            // SetUp
+            Initial();
+            _graphics.Ini
+            
+            // Act
+            
+            
+            // Assert
+            // Assert.NotNull(e.Graphics);
+        }
+
+        private void CreateGraphics_Paint(object sender, PaintEventArgs e)
+        {
+            // SetUp
+            var chartManager = new SmithChart();
+            
+            // Act
+            chartManager.DrawGraphics(e, _arg1, _arg2);
+        }
+    }
+}*/
